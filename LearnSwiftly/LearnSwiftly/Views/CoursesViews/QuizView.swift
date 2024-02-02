09a7415a6b93b217a1b553d3f4d08a8e8b75f74c
Manajit Halder <manajit.halder@gmail.com>
2024-02-02 22:25:32 +0530

    Working on QuizView

--- LearnSwiftly/LearnSwiftly/Views/CoursesViews/QuizView.swift
+++ LearnSwiftly/LearnSwiftly/Views/CoursesViews/QuizView.swift
@@ -8,21 +8,66 @@ import SwiftUI
 
 struct QuizView: View {
     let quiz: Quiz
+    @State private var currentIndex: Int = 0
+//    @State private var userAnswer: String = ""
+//    @State private var isQuizSubmitted: Bool = false
     
     var body: some View {
         VStack {
-            Text(quiz.question)
-            
-            ForEach(quiz.options, id: \.self) { option in
-                Text(option)
+            if currentIndex < quiz.options.count {
+                Text(quiz.question)
+                
+                ForEach(quiz.options.indices, id: \.self) { index in
+                    if index < quiz.options.count - 1 {
+//                        currentIndex = index + 1
+                        NavigationLink(destination: OptionView(item: quiz.options[index], nextIndex: currentIndex), label: {
+                            Text(quiz.options[index])
+                        })
+                    }
+                }
+                
+            } else {
+                Text("End of Quiz")
+                    .padding()
             }
-            
-            
+        }
+        .padding()
+        
+    }
+    
+}
+
+ 
+struct OptionView: View {
+    let item: String
+    var nextIndex: Int
+    @State private var userAnswer = ""
+
+    var body: some View {
+        VStack {
+            Text(item)
+            TextField("Your Answer", text: $userAnswer)
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+            Button("Submit") {
+                // Handle the user's answer, and navigate to the next item
+                // For simplicity, let's just print the answer and update the index
+                print("User Answer: \(userAnswer)")
+                // You can implement your logic here based on the answer
+                // For now, just move to the next item
+//                nextIndex += 1
+            }
+            .padding()
         }
         .padding()
     }
 }
-
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
+                                   
 struct QuizView_Previews: PreviewProvider {
     static var previews: some View {
         QuizView(quiz: Quiz(isCorrect: false))

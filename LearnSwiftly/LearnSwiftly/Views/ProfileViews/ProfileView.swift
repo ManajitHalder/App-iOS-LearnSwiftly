70f10070a8acc6b690d8e3e308d4133e901cb719
Manajit Halder <manajit.halder@gmail.com>
2024-02-21 22:07:56 +0530

    Completed Profile view

--- LearnSwiftly/LearnSwiftly/Views/ProfileViews/ProfileView.swift
+++ LearnSwiftly/LearnSwiftly/Views/ProfileViews/ProfileView.swift
@@ -13,22 +13,11 @@ struct ProfilePhotoView: View {
     var body: some View {
         ZStack {
             VStack {
-                ZStack {
-                    Rectangle()
-                        .frame(width: 190)
-                        .foregroundColor(.red)
-                    
-                    Rectangle()
-                        .frame(width: 170)
-                         .foregroundColor(.white)
-                                        
-//                    Image(systemName: "heart.fill")
-                    Image(uiImage: UIImage(contentsOfFile: "/Volumes/Mojave/Users/reyansh/Data/01 Reyansh/Photos/DSC_1400.JPG")!)
-                        .resizable()
-                        .scaledToFill()
-                        .frame(width: 120)
-//                        .foregroundColor(.green)
-                }
+                Image(uiImage: UIImage(contentsOfFile: "/Volumes/Mojave/Users/reyansh/Data/01 Reyansh/Photos/DSC_1400.JPG")!)
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 200, height: 180)
+                    .cornerRadius(10.0)
                 
                 Text("Reyansh Halder")
                     .foregroundColor(.white)
@@ -108,9 +97,165 @@ struct CertificateView: View {
     }
 }
 
-struct ProfileView: View {
+struct PremiumCourseView: View {
     @State private var isPremiumCourseSelected: Bool = false
     
+    var body: some View {
+        ZStack {
+            Rectangle()
+            
+            VStack {
+                VStack {
+                    Text("Unlock Premium Courses")
+                        .font(.title)
+                        .foregroundColor(.white)
+                        .multilineTextAlignment(.leading)
+                    
+                    Text("Unlock unlimited benefits with premium courses")
+                        .font(.title3)
+                        .foregroundColor(.gray)
+                        .multilineTextAlignment(.leading)
+                }
+                
+                HStack {
+                    Button {
+                        isPremiumCourseSelected.toggle()
+                    } label: {
+                        Text("Upgrade to Premium")
+                            .fontWeight(.bold)
+                    }
+                    .buttonBorderShape(.automatic)
+                    .buttonStyle(.borderedProminent)
+                    .foregroundColor(.white)
+                    .padding(.bottom, 20)
+                    
+                    .sheet(isPresented: $isPremiumCourseSelected) {
+                        PremiumPaymentSheetView(isPremiumCourseSelected: $isPremiumCourseSelected)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+            .padding()
+        }
+        .frame(height: 200)
+        .foregroundColor(.purple)
+        .cornerRadius(20)
+    }
+}
+
+struct ActivityViewController: UIViewControllerRepresentable {
+    typealias UIViewControllerType = UIActivityViewController
+
+    let activityItems: [Any]
+    var preferredContentSize: CGSize // Define a preferred content size
+
+    func makeUIViewController(context: Context) -> UIActivityViewController {
+        let activityViewController = UIActivityViewController(activityItems: activityItems, applicationActivities: nil)
+        activityViewController.preferredContentSize = preferredContentSize // Set the preferred content size
+        return activityViewController
+    }
+
+    func updateUIViewController(_ uiViewController: UIActivityViewController, context: Context) {
+        // Update
+    }
+}
+
+struct InviteFriendSheetView: View {
+    @Binding var isFriendInviteSent: Bool
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Button {
+                    isFriendInviteSent.toggle()
+                } label: {
+                    Image(systemName: "xmark")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                }
+                .padding()
+                
+                Spacer()
+            }
+            
+            Spacer()
+            
+            Text("Install & Learn http://play.google.com/store/apps")
+            
+            Spacer()
+        }
+        .presentationDetents([.fraction(0.70)])
+        .presentationDragIndicator(.hidden)
+        .presentationCornerRadius(20.0)
+        .presentationBackground(Color.gray)
+    }
+}
+
+struct InviteFriendView: View {
+    @State private var isFriendInviteSent: Bool = false
+    let sharedText = "Checkout this app @ http://play.google.com/store/apps"
+    
+    var body: some View {
+        ZStack {
+            Rectangle()
+            
+            HStack() {
+                Image(systemName: "figure.stand.line.dotted.figure.stand")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 70)
+                    .multilineTextAlignment(.leading)
+                    .foregroundColor(.red)
+                    .padding([.leading, .trailing], 10)
+                
+                VStack {
+                    HStack {
+                        Text("Invite Friends")
+                            .font(.title)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.leading)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Text("Share your learnings with your friends")
+                            .font(.title3)
+                            .foregroundColor(.gray)
+                            .multilineTextAlignment(.leading)
+                        
+                        Spacer()
+                    }
+                    
+                    HStack {
+                        Button {
+                            isFriendInviteSent.toggle()
+                        } label: {
+                            Text("Invite Friends")
+                                .fontWeight(.semibold)
+                        }
+                        .buttonBorderShape(.automatic)
+                        .buttonStyle(.borderedProminent)
+                        .foregroundColor(.white)
+                        .padding(.bottom, 10)
+                        
+                        .sheet(isPresented: $isFriendInviteSent, content: {
+//                            InviteFriendSheetView(isFriendInviteSent: $isFriendInviteSent)
+                            ActivityViewController(activityItems: [sharedText], preferredContentSize: CGSize(width: 300, height: 400))
+                        })
+                        
+                        Spacer()
+                    }
+                }
+            }
+        }
+        .frame(height: 180)
+        .foregroundColor(Color.orange)
+        .cornerRadius(20)
+    }
+}
+
+struct ProfileView: View {
     var body: some View {
         LinearGradient(
             gradient: Gradient(colors: [Color("screenBackground")]),
@@ -131,48 +276,13 @@ struct ProfileView: View {
                         
                         ScoreView()
                         
-                        CertificateView()
+//                        CertificateView()
                         
                         // Premium Course View
-                        ZStack {
-                            Rectangle()
-                            
-                            VStack {
-                                VStack {
-                                    Text("Unlock Premium Courses")
-                                        .font(.title)
-                                        .foregroundColor(.white)
-                                        .multilineTextAlignment(.leading)
-                                    
-                                    Text("Unlock unlimited benefits with premium courses")
-                                        .font(.title3)
-                                        .foregroundColor(.gray)
-                                        .multilineTextAlignment(.leading)
-                                }
-                                
-                                HStack {
-                                    Button {
-                                        isPremiumCourseSelected.toggle()
-                                    } label: {
-                                        Text("Upgrade to Premium")
-                                            .fontWeight(.bold)
-                                    }
-                                    .buttonBorderShape(.automatic)
-                                    .buttonStyle(.borderedProminent)
-                                    .foregroundColor(.white)
-                                    .padding(.bottom, 20)
-                                    
-                                    .sheet(isPresented: $isPremiumCourseSelected) {
-                                        PremiumPaymentSheetView(isPremiumCourseSelected: $isPremiumCourseSelected)
-                                            .foregroundColor(.white)
-                                    }
-                                }
-                            }
-                            .padding()
-                        }
-                        .frame(height: 200)
-                        .foregroundColor(.purple)
-                        .cornerRadius(20)
+                        PremiumCourseView()
+                        
+                        // Invite a friend
+                        InviteFriendView()
                         
                     }
                     .padding()

d17ad5b9e3eab4e07863e20e71bf452d1752cd96
Manajit Halder <manajit.halder@gmail.com>
2024-02-27 23:27:03 +0530

    Corrected Content.swift as per swagger file changes

--- LearnSwiftly/LearnSwiftly/Models/Content.swift
+++ LearnSwiftly/LearnSwiftly/Models/Content.swift
@@ -6,20 +6,39 @@
 
 import Foundation
 
-enum ContentType: Codable {
-    case subtitle(String)
-    case text(String)
-    case code(String)
-    case image(String)
+//enum ContentType: Codable {
+//    case subtitle(String)
+//    case text(String)
+//    case code(String)
+//    case image(String)
+//}
+//
+//struct ContentElement: Identifiable, Codable {
+//    let id: UUID
+//    let type: ContentType
+//    
+//    init(id: UUID = UUID(), type: ContentType) {
+//        self.id = id
+//        self.type = type
+//    }
+//}
+
+enum ContentType: String, Codable {
+    case subtitle
+    case text
+    case code
+    case image
 }
 
 struct ContentElement: Identifiable, Codable {
     let id: UUID
     let type: ContentType
+    let data: String
     
-    init(id: UUID = UUID(), type: ContentType) {
+    init(id: UUID = UUID(), type: ContentType, data: String) {
         self.id = id
         self.type = type
+        self.data = data
     }
 }
 
@@ -43,65 +62,65 @@ extension Content {
     static let content11 = Content(
         title: "Basics of Swift Programming",
         elements: [
-            ContentElement(type: .subtitle("Topic Name 1")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .subtitle("Topic Name 2")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .code("struct ContentView: View {\n    var body: some View {\n        Text(\"Hello, SwiftUI, How are you doing in my app fksdfds fldsjf sdjfdlsj fkldsjfk jdsklfjsdlkfjsdklfjd dkslfjklds jfkldsjfklsdjfkljsd\n"))
+            ContentElement(type: .subtitle, data: "Topic Name 1"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .subtitle, data: "Topic Name 2"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .code, data: "struct ContentView: View {\n    var body: some View {\n        Text(\"Hello, SwiftUI, How are you doing in my app fksdfds fldsjf sdjfdlsj fkldsjfk jdsklfjsdlkfjsdklfjd dkslfjklds jfkldsjfklsdjfkljsd\n")
         ],
-        quiz: Quiz.sampleQuiz)
+        quiz: [Quiz.quiz1, Quiz.quiz2, Quiz.quiz3, Quiz.quiz4])
     
     static let content12 = Content(
         title: "Variables in Swift",
         elements: [
-            ContentElement(type: .subtitle("Topic Name 1")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .subtitle("Topic Name 2")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .code("print()\nprint\nprint\n"))
+            ContentElement(type: .subtitle, data: "Topic Name 1"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .subtitle, data: "Topic Name 2"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .code, data: "print()\nprint\nprint\n")
         ],
-        quiz: Quiz.sampleQuiz)
+        quiz: [Quiz.quiz1, Quiz.quiz2, Quiz.quiz3, Quiz.quiz4])
     
     static let content13 = Content(
         title: "Strings in Swift",
         elements: [
-            ContentElement(type: .subtitle("Topic Name 1")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .subtitle("Topic Name 2")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .code("print()\nprint\nprint\n"))
+            ContentElement(type: .subtitle, data: "Topic Name 1"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .subtitle, data: "Topic Name 2"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .code, data: "print()\nprint\nprint\n")
         ],
-        quiz: Quiz.sampleQuiz)
+        quiz: [Quiz.quiz1, Quiz.quiz2, Quiz.quiz3, Quiz.quiz4])
     
     static let content21 = Content(
         title: "Optionals",
         elements: [
-            ContentElement(type: .subtitle("Topic Name 1")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .code("print()\nprint\nprint\n"))
+            ContentElement(type: .subtitle, data: "Topic Name 1"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .code, data: "print()\nprint\nprint\n")
         ],
-        quiz: Quiz.sampleQuiz)
+        quiz: [Quiz.quiz1, Quiz.quiz2, Quiz.quiz3, Quiz.quiz4])
     
     static let content22 = Content(
         title: "Closures with examples",
         elements: [
-            ContentElement(type: .subtitle("Topic Name 1")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .code("print()\nprint\nprint\n"))
+            ContentElement(type: .subtitle, data: "Topic Name 1"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .code, data: "print()\nprint\nprint\n")
         ],
-        quiz: Quiz.sampleQuiz)
+        quiz: [Quiz.quiz1, Quiz.quiz2, Quiz.quiz3, Quiz.quiz4])
     
     static let content23 = Content(
         title: "Multithreading",
         elements: [
-            ContentElement(type: .subtitle("Topic Name 1")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .subtitle("Topic Name 2")),
-            ContentElement(type: .text("In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application.")),
-            ContentElement(type: .code("print()\nprint\nprint\n"))
+            ContentElement(type: .subtitle, data: "Topic Name 1"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .subtitle, data: "Topic Name 2"),
+            ContentElement(type: .text, data: "In this enum, serves as a clear indication that the task has not been initiated. Choose the term that aligns best with the language and context used in your application."),
+            ContentElement(type: .code, data: "print()\nprint\nprint\n")
         ],
-        quiz: Quiz.sampleQuiz)
+        quiz: [Quiz.quiz1, Quiz.quiz2, Quiz.quiz3, Quiz.quiz4])
 }
 
